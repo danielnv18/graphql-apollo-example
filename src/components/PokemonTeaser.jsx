c709f25952f104579a387c132a6c51a5016216bc
Daniel Noyola <daniel.noyola.dev@gmail.com>
2021-10-10 19:16:37 -0600

    Link to pokemon.

--- src/components/PokemonTeaser.jsx
+++ src/components/PokemonTeaser.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
 import { getIdFromUrl } from '../utils';
 import { getPokemonImageDetail } from '../utils/image';
 import clasess from '../styles/app.module.css';
@@ -8,12 +10,23 @@ const PokemonItem = ({ name, url }) => {
   const imgageUrl = getPokemonImageDetail(id);
   return (
     <div>
-      <figure className={clasess.pokemonTeaser}>
-        <img height={205} width={205} src={imgageUrl} alt={name} />
-      </figure>
-      <span>
-        #{id.padStart(3, 0)} - {name[0].toUpperCase() + name.substring(1)}
-      </span>
+      <Link to={`/pokedex/${id}`}>
+        <figure className={clasess.pokemonTeaser}>
+          <img
+            height={205}
+            width={205}
+            src={imgageUrl}
+            alt={name}
+            loading="lazy"
+          />
+        </figure>
+      </Link>
+
+      <Link to={`/pokedex/${id}`} className={clasess.pokemonTeaserLink}>
+        <h5>
+          #{id.padStart(3, 0)} - {name[0].toUpperCase() + name.substring(1)}
+        </h5>
+      </Link>
     </div>
   );
 };
